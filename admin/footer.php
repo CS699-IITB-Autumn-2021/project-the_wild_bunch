82d31b50bcccccdeeeec9c65f637817e4e2dc8e9
kamal kakadiya <39611675+kamal153@users.noreply.github.com>
2021-10-06 08:25:39 +0530

    Delete admin directory

--- admin/footer.php
+++
@@ -1,9 +0,0 @@
-</div>
-    <script type="text/javascript">
-        $("#dataTable").DataTable();
-        $('#dataTable').removeClass( 'display' ).addClass('table table-striped table-bordered');
-        
-    </script>
-    </script>
-</body>
-</html>
