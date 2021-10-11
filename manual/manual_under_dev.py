9f647bd7c8e3c2c422504309be8ffa0338ff9bae
anirjoshi <170020004@iitdh.ac.in>
2021-10-11 20:29:56 +0530

    used class DY5T1d RZIKme to get title and url

--- manual/manual_under_dev.py
+++ manual/manual_under_dev.py
@@ -34,33 +34,17 @@ print("opened url...complete")
 
 # print(webpage)
 
-#start session
-with requests.Session() as c:
+
+#use beautiful soup
+soup = BeautifulSoup(webpage, 'lxml')
+
+#print formatted output
+print(soup.prettify())
+print("-----------------------------------")
+
+for i in (soup.find_all('a',class_="DY5T1d RZIKme")):
+ 	print(i.text,root+i['href'][2:],sep="\t")
+ 	# print(i)
+ 	print()
 	
-	#use beautiful soup
-	soup = BeautifulSoup(webpage, 'html5lib')
-	
-	#print formatted output
-	print(soup.prettify())
-	print("-----------------------------------")
-	
-	#This div class kCrYT contains the title and the link of article
-	for item in soup.find_all('div',class_='kCrYT'):
-		#start a try block as there may be no entity <a> in the class
-		try:
-			
-			#print the url whole tag
-	 		print(item.a)
-	 		
-	 		#print the url link in the whole tag and the title of the article separated by tab
-	 		print(item.a['href'],item.a.find('div',class_="BNeawe vvjwJb AP7Wnd").text,sep='\t')
-	 		
-	 		
-		except:
-	 		#if there is no div class or url then just print the tag
-	 		print(":::",item.a,(item.a.text),sep='\t')
-	 		print("~~~",item)
-	 		pass
-	
-	print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 	
