e5afa4f08cbd0ef560e7f96908882b5b8c2df545
anirjoshi <170020004@iitdh.ac.in>
2021-10-11 18:50:17 +0530

    comments added

--- news_api.py
+++ news_api.py
@@ -3,27 +3,26 @@ import sys
 # Init
 newsapi = NewsApiClient(api_key='b29a84ec447f48cfbc0fd00def991e69')
 
-# /v2/top-headlines
-# top_headlines = newsapi.get_top_headlines(q='bitcoin',
-#                                           sources='bbc-news,the-verge',
-#                                           category='business',
-#                                           language='en',
-#                                           country='us')
-
-# # /v2/everything
+#see if command line input word is passed or not
 if len(sys.argv)>1:
 	term = sys.argv[1]
 else:
+	#if no command line argument then use bitcoin as word
 	term = 'bitcoin'
 
+#search from bbc news
 all_articles = newsapi.get_everything(q=term, sources='bbc-news,the-verge', domains='bbc.co.uk,techcrunch.com',language='en',page=2)	
 
-# # /v2/top-headlines/sources
-# sources = newsapi.get_sources()
+#print status of articles
 print(all_articles['status'])
+
+#print total number of results
 print(all_articles['totalResults'])
+
+#iterate through the returned articles
 for i in all_articles['articles']:
+	#print title description url and image url
 	print(i['title'], i['description'] , i['url'],i['urlToImage'], sep='\t')
 	
-	
+##this is paid! and gives restriced number of responses.
 ##ref:https://newsapi.org/docs/client-libraries/python
