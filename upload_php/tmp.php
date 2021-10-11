6acb1b38c7acabc080b6cadcf6c6674b45d793fd
anirjoshi <170020004@iitdh.ac.in>
2021-10-11 23:34:33 +0530

    comments added on upload_php files

--- upload_php/tmp.php
+++ upload_php/tmp.php
@@ -1,7 +1,11 @@
 <?php 
 
+//create command on shell to run webscraping
 $command = escapeshellcmd('python ./manual_under_dev.py');
+//execute command on shell to run webscraping
 $output = shell_exec($command);
+
+//print output
 echo $output;
 
 ?>
--- upload_php/upload_php.php
+++ upload_php/upload_php.php
@@ -12,20 +12,24 @@
 	//get the extension of the uploaded file
 	$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 	
-	// Check if image file is a actual image or fake image
+	//check if submit field is set
 	if(isset($_POST["submit"])) {
 		$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+		//check if the image is appropriate
 		if($check !== false) {
 			echo "File is an image - " . $check["mime"] . ".";
 			echo "<img src=".$_FILES["fileToUpload"]["tmp_name"]." height=200 width=300 />";
 			$uploadOk = 1;
 
+			//connect to sql server
 			$host ="localhost:3306"; 
 			$username ="root";
 			$password ="ani";
 			$database ="news_proj";
 
 			$x=mysqli_connect($host, $username, $password, $database);
+
+			//execute query on sql server
 			$result=mysqli_query($x,"select tags from all_tags");
 			while($row=mysqli_fetch_row($result)){
 				$y=$y."<tr><td>".$row[0]."</td></tr>";
@@ -33,6 +37,7 @@
 				echo "<table border='2'>
 				<tr><th>tags</th></tr>". $y."</table>";
 			}
+			//close sql connection
 			mysqli_close($connect);
 	  	} 
 	  	else {
