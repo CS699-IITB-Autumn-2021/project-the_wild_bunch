233a36fe468b02d59df19a0a2461b22feecf377a
anirjoshi <170020004@iitdh.ac.in>
2021-10-11 18:40:06 +0530

    comments added, image issue

--- g_news.py
+++ g_news.py
@@ -1,17 +1,26 @@
 
 import sys
 from GoogleNews import GoogleNews
+
+#create object
 googlenews = GoogleNews()
+
+#set language
 googlenews = GoogleNews(lang='en')
 
+#see if command line input word is passed or not
 if len(sys.argv)>1:
 	term = sys.argv[1]
 else:
+	#if no command line argument then use APPLE as word
 	term = "APPLE"
 
 googlenews.search(term)
 # print(googlenews.results()[0])
 
 for articles in googlenews.results():
-	print(articles['title'],articles['link'],articles['img'],sep='\t')
-
+	print(articles['title'],articles['desc'],articles['link'],sep='\t')
+	#get texts here?
+	
+###image not working here, this is also given on their website
+#https://pypi.org/project/GoogleNews/
