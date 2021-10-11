10ab72ba67f87e44d9f794f542b42972a3a69184
anirjoshi <170020004@iitdh.ac.in>
2021-10-11 20:26:22 +0530

    div class kCrYT doesn't work with new link

--- manual/manual_under_dev.py
+++ manual/manual_under_dev.py
@@ -64,26 +64,3 @@ with requests.Session() as c:
 	
 	print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 	
-	#This div class kCrYT contains the title and the link of article
-	for i in soup.find_all('div',class_='kCrYT'):
-		
-		#find the url in this <div> tag
-		if (type(i.find('a')))!=type(None):
-			
-			#fetch the url and print it
-			print(i.find('a')['href'].split("/url?q=")[1], end="\t")
-			# print(i)
-			
-			#get the textual title
-			text_data_list = (re.split('\d+ hours ago . ', i.text))
-			text_data = ""
-			if len(text_data_list)==2:
-				text_data=text_data_list[1]
-			else:
-				text_data=text_data_list[0]
-			
-			#print title
-			print(text_data)
-			
-			#print separator for easy readibility of the output
-			print("++++++++++++++++++++++++")
