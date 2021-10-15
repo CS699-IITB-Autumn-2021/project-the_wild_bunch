76910e72eb090720389ad4430561acc4e8f31e26
anirjoshi <170020004@iitdh.ac.in>
2021-10-15 12:05:32 +0530

    multiple keywords search updated on merge

--- merge_info/tmp.php
+++ merge_info/tmp.php
@@ -6,10 +6,12 @@ function parse_keyform($str) {
     $big = "";
     foreach ($chars as $char) {
         // echo $char;
-        if ($char == " "){
-            $big = $big."\ ";
+        if ($char == '"'){
+            $big = $big.'\"';
         }
-        $big = $big.$char;
+        else{
+        	$big = $big.$char;
+        }
         // echo "<br>";
     }
     return $big;
@@ -17,15 +19,19 @@ function parse_keyform($str) {
 
 echo "<html>
 <body>";
+$output=null;
+$retval=null;
+//echo shell_exec("whoami");
 //echo "Keyword Searched for: ".$_POST["keyword"];
 //$keyword = parse_keyform($_POST["keyword"]);
-$keyword = escapeshellarg($_POST["keyword"]);
-echo "<br>Keyword after processing: ".$keyword;
-
-$command = escapeshellcmd('./virt_env/bin/python ./manual_under_dev.py -k '.$keyword);
+$keyword = parse_keyform($_POST["keyword"]);
+echo "<br>Keyword after processing: ".$keyword."<br>";
+$command_exec ="./virt_env/bin/python ./manual_under_dev.py -k \"".$keyword."\"";
+echo $command_exec;
+$command = shell_exec($command_exec);
 //echo "<br>".$keyword;
-echo "<br>Command Executed: ".$command;
-$output = shell_exec($command);
+echo "<br>Command Executed: ".$command."<br>";
+//$output = shell_exec($command);
 echo $output;
 echo "</body>
 </html>";
