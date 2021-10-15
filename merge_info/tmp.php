52f006d67ccc972d247a378dcb46557cc9051c34
anirjoshi <170020004@iitdh.ac.in>
2021-10-15 10:12:49 +0530

    tmp.php modified to have multiple words

--- merge_info/tmp.php
+++ merge_info/tmp.php
@@ -1,8 +1,30 @@
 <?php 
+
+function parse_keyform($str) {
+    // echo "Hello world!";
+    $chars = str_split($str);
+    $big = "";
+    foreach ($chars as $char) {
+        // echo $char;
+        if ($char == " "){
+            $big = $big."\ ";
+        }
+        $big = $big.$char;
+        // echo "<br>";
+    }
+    return $big;
+}
+
 echo "<html>
 <body>";
-echo "Keyword Searched for: ".$_POST["keyword"];
-$command = escapeshellcmd('./virt_env/bin/python ./manual_under_dev.py '.$_POST["keyword"]);
+//echo "Keyword Searched for: ".$_POST["keyword"];
+//$keyword = parse_keyform($_POST["keyword"]);
+$keyword = escapeshellarg($_POST["keyword"]);
+echo "<br>Keyword after processing: ".$keyword;
+
+$command = escapeshellcmd('./virt_env/bin/python ./manual_under_dev.py -k '.$keyword);
+echo "<br>".$keyword;
+echo "<br>command".$command;
 $output = shell_exec($command);
 echo $output;
 echo "</body>
