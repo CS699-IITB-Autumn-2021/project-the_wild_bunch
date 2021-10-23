9e2e42738aefaed0b6e351efe3241b191e64f251
anirjoshi <170020004@iitdh.ac.in>
2021-10-23 18:43:00 +0530

    arguments added, fetching changed

--- manual/manual_under_dev.py
+++ manual/manual_under_dev.py
@@ -6,6 +6,44 @@ import re
 import sys
 import parse_class as ps
 import time
+import argparse
+
+
+def key_word_process(keyword, file_mapping):
+	"""This is the process keyword function.
+	It takes as input keyword which is the
+	characters given by the user to search and
+	file_mapping which is the name of the file
+	which contains mapping from character into special
+	characters. This function substitutes those characters
+	with special characters with the same mapping.
+
+	Parameters
+	----------
+	keyword: String
+	file_mapping: String.
+
+	Returns
+	-------
+	n_keyword: String
+	"""
+	words_mapping= {}
+	with open(file_mapping) as f:
+		for line in f:
+			key = line[0]
+			val = line.split()[-1]
+			words_mapping[key] = val
+	# print(words_mapping)
+    # exit()
+	n_keyword = ""
+	for i in range(0,len(keyword)):
+        # print(keyword[i], )
+		if keyword[i] in words_mapping.keys():
+			n_keyword = n_keyword + words_mapping[keyword[i]]
+		else:
+			n_keyword = n_keyword + keyword[i]
+	return n_keyword
+
 def main():
 	"""This is the main
 	Get keyword to search, through
@@ -25,12 +63,26 @@ def main():
 	None
 	"""
 	
-	#default keyword is Trump
-	key_word = "Trump"
+	
 
 	#use the keyword passed in commandline argument if any
-	if len(sys.argv)>1:
-		key_word=sys.argv[1]
+	# if len(sys.argv)>1:
+	# 	key_word=sys.argv[1]
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-k","--keyword",metavar="", required=False, help="Keyword to search")
+	parser.add_argument("-f","--special_character_mapping",metavar="", required=False, help="File name of special characters")
+	args = parser.parse_args()
+
+	file_mapping = "map.txt"
+	if args.special_character_mapping:
+		file_mapping = args.special_character_mapping
+    
+	#default keyword is Trump
+	key_word = "Trump"
+	if args.keyword:
+		key_word = args.keyword
+
+	key_word = key_word_process(key_word, file_mapping)
 
 	#the root link
 	root = "https://www.google.com/"
@@ -39,25 +91,27 @@ def main():
 	link = "https://news.google.com/search?q="+key_word+"&hl=en-IN&gl=IN&ceid=IN:en"
 
 	#request for the link
-	req = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
+	#req = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
 
 	#add a print statement to have more interaction
 	print("opening url...", link)
-
+	
+	source = requests.get(link).text
+	soup = BeautifulSoup(source, 'lxml')
 
 	#read the webpage
-	webpage = urlopen(req).read()
+	#webpage = urlopen(req).read()
 
 	#add a print statement to have more interaction
 	print("opened url...complete")
-	print()
-	print()
+	# print()
+	# print()
 
 	# print(webpage)
 
 
 	#use beautiful soup
-	soup = BeautifulSoup(webpage, 'lxml')
+	#soup = BeautifulSoup(webpage, 'lxml')
 
 	#print formatted output
 	#print(soup.prettify())
@@ -73,13 +127,14 @@ def main():
 	article_obj = ps.param_matcher(str(soup.find_all('script')[17].text))
 
 	#print only the nested [] in the tmp file name
-	article_obj.rem_brackets("tmp_"+key_word+"_rm_brackets_data.txt")
+	# article_obj.rem_brackets("tmp_"+key_word+"_rm_brackets_data.txt")
 
 	#print all the textual data prettily in the file
-	article_obj.pretty_print("tmp_"+key_word+"_rm_pretty.txt")
+	# article_obj.pretty_print("tmp_"+key_word+"_rm_pretty.txt")
 
 	time.sleep(5)
-	article_obj.parser_to_print()
+	article_obj.populate_data()
+	article_obj.html_print()
 
 
 if __name__ == "__main__":
