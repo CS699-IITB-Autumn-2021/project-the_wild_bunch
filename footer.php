e992bf79b508e18120fd09c15f58336a98903173
kamal kakadiya <39611675+kamal153@users.noreply.github.com>
2021-10-23 20:18:55 +0530

    Delete footer.php

--- footer.php
+++
@@ -1,9 +0,0 @@
-</div>
-    <!-- Java scipt for Datatable -->
-    <script type="text/javascript">
-        $("#dataTable").DataTable();
-        $('#dataTable').removeClass( 'display' ).addClass('table table-striped table-bordered');
-    </script>
-    </script>
-</body>
-</html>
