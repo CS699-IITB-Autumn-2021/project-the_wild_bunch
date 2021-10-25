820d04fd158bb774f561994b0d938d4f538dc9dd
Animesh <35520996+Animesh98@users.noreply.github.com>
2021-10-26 01:40:56 +0530

    Add files via upload

--- admin/index.php
+++ admin/index.php
@@ -580,20 +580,4 @@
         </div>
     </div>
 </div>
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Add Admin</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        You are not authorised to add admin.
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
 <?php include_once 'footer.php'; ?>
